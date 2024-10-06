6bd72e7c342d650153c1dd47c842d4d3ef5b6a7a
margostino <maj.dagostino@gmail.com>
2024-10-06 17:58:57 +0200

    wip

--- autobox/metrics/prometheus.py
+++ autobox/metrics/prometheus.py
@@ -6,13 +6,15 @@ from autobox.schemas.metrics import Metric
 
 
 def metric_exists(metric_name):
-    return any(metric.name == metric_name for metric in REGISTRY.collect())
+    for collector in REGISTRY._collector_to_names.items():
+        if metric_name in collector[1]:
+            return True
+    return False
 
 
 def create_prometheus_metrics(metrics: Dict[str, Metric]):
     labels = ["simulation_id", "simulation_name"]
     for name, metric in metrics.items():
-        # TODO: fix it
         if not metric_exists(name):
             if metric.type == "counter":
                 metric.collector_registry = Counter(name, metric.description, labels)
